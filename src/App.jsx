1b630773a193dacdad0e671f7f1b9f01fcb51335
Nazanin Moosazade <MY_NAME moosazadenazanin@gmail.com>
2024-07-10 23:53:44 +0330

    add all

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@ import React from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
 import Sidebar from './Components/Sidebar/Sidebar'
+import { useRoutes } from 'react-router-dom'
+import routes from './routes'
 
 export default function App() {
+
+ const router = useRoutes(routes)
+
   return (
     <>
     <Header/>
-    <Sidebar/>
+    <div class="container px-0">
+        <main class="main">
+          <div class="row justify-content-between mx-0">
+          <Sidebar/>
+          {router}
+          </div>
+        </main>
+      </div>
     </>
   )
 }
