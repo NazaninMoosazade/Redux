dadf0cf2f62cb16fb42a3c0ab613dbb0a6dcde31
Nazanin Moosazade <MY_NAME moosazadenazanin@gmail.com>
2024-07-10 23:23:00 +0330

    add sidebar in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
+import Sidebar from './Components/Sidebar/Sidebar'
 
 export default function App() {
   return (
     <>
     <Header/>
+    <Sidebar/>
     </>
   )
 }
