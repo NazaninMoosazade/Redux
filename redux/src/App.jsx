379bf05cdbefebe926e93ea4bd3498829767bcf2
Nazanin Moosazade <MY_NAME moosazadenazanin@gmail.com>
2024-07-10 13:51:42 +0330

    add header jsx

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Header from './Components/Header/Header'
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+    <Header/>
+    </>
   )
 }
