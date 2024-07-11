32cd5d5e3aebfa6bb243b16382f0e9959c10574c
Nazanin Moosazade <MY_NAME moosazadenazanin@gmail.com>
2024-07-11 15:16:19 +0330

    create Users.jsx

---
+++ src/Redux/store/Users.jsx
@@ -0,0 +1,11 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+
+const slice = createSlice({
+    name: 'users' ,
+    initialState: [] ,
+    reducers: {
+
+    }
+})
+
+export default slice.reducer;
