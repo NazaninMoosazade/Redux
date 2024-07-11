3eac8842c9350d22f068edbcec76ba9276d9335d
Nazanin Moosazade <MY_NAME moosazadenazanin@gmail.com>
2024-07-11 13:49:47 +0330

    add courses in routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 import Users from "./Pages/Users/Users";
+import Courses from "./Pages/Courses/Courses";
 
 export default [
    {path: "/" , element: <Users/>},
    {path: "/users" , element: <Users/>},
-
+   {path: './courses' , element: <Courses/>},
 ];
