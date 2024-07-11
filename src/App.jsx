9a11ae49e0a6bffe222b7887c8c49031a128337a
Nazanin Moosazade <MY_NAME moosazadenazanin@gmail.com>
2024-07-11 14:07:48 +0330

    create courses

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import './App.css'
 import Header from './Components/Header/Header'
 import Sidebar from './Components/Sidebar/Sidebar'
 import { useRoutes } from 'react-router-dom'
 import routes from './routes'
+
+import './App.css'
 
 export default function App() {
 
--- src/routes.jsx
+++ src/routes.jsx
@@ -4,7 +4,7 @@ import Users from "./Pages/Users/Users";
 import Courses from "./Pages/Courses/Courses";
 
 export default [
-   {path: "/" , element: <Users/>},
-   {path: "/users" , element: <Users/>},
-   {path: './courses' , element: <Courses/>},
-];
+   { path: "/", element: <Users /> },
+   { path: "/users", element: <Users /> },
+   { path: "/courses", element: <Courses /> },
+ ];
